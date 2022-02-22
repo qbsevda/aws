b7c3efced3b8ed35604036cb434caf3de14dbf77
qbsevda <qbsevda@gmail.com>
2022-02-21 20:41:53 -0500

    projects update

--- Linux/projects/Project-101/First_Part/search.sh
+++ Linux/projects/Project-101/First_Part/search.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-search_user() {
-        awk -F, '{ if ($1 == "serdar" && $4 == "TerminateInstances") print $0 }' event_history.csv > result.txt
-        #sleep 2
-        awk -F "\"\"" '{ print $8 }' result.txt > result.tmp && mv result.tmp result.txt
-        }
-search_user
+​
+echo -e "Bu script *nightfighter* AWS-DevOps Grubu tarafindan yapilmiştir.\n"
+​
+cat event_history.csv | grep -i "serdar" | grep -i "terminateinstance"  | awk -F'"' '{print $16}' | cat >> serdar.txt
+​
+cat event_history.csv | grep -i "serdar" | grep -i "terminateinstance"  | awk -F'"' '{print $32}' | cat >> serdar.txt
+​
+cat event_history.csv | grep -i "serdar" | grep -i "terminateinstance"  | awk -F'"' '{print $48}' | cat >> serdar.txt
+​
+cat event_history.csv | grep -i "serdar" | grep -i "terminateinstance"  | awk -F'"' '{print $64}' | cat >> serdar.txt
+​
+cat event_history.csv | grep -i "serdar" | grep -i "terminateinstance"  | awk -F'"' '{print $80}' | cat >> serdar.txt
+​
+cat event_history.csv | grep -i "serdar" | grep -i "terminateinstance"  | awk -F'"' '{print $96}' | cat >> serdar.txt
+​
+sort serdar.txt | uniq | tee serdar.txt
