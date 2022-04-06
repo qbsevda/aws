d0bd96926cb9e804f7374afea8ba7a216e060020
qbsevda <qbsevda@gmail.com>
2022-04-06 17:23:41 -0400

    update

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.69.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  # access_key = "my-access-key"
+  # secret_key = "my-secret-key"
+  ## profile = "my-profile"
+}
+
+resource "aws_instance" "tf-ec2" {
+  ami           = "ami-0ed9277fb7eb570c9"
+  instance_type = "t2.micro"
+  tags = {
+    "Name" = "created-by-tf"
+  }
+}
